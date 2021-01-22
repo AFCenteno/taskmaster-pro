acd1f4d637bbfdfdda3f85b3b36da384a0011418
mrafcent1 <alexfcenteno@outlook.com>
2021-01-22 18:33:30 -0500

    date features

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,9 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //audit taskli
+  auditTask(taskLi);
+
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -102,12 +105,19 @@ $(".list-group").on("click", "span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
+
   // automatically focus on new element
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -135,6 +145,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
   
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // modal was triggered
@@ -169,6 +182,33 @@ $("#task-form-modal .btn-primary").click(function() {
 
     saveTasks();
   }
+});
+
+//audit
+var auditTask = function(taskEl) {
+  //get date from taskEl
+  var date = $(taskEl).find("span").text().trim();
+  console.log(date);
+
+  //convert to moment object at 5PM 
+  var time = moment(date, "L").set("hour", 17);
+  console.log(time)
+
+  //remove any old classes
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task in near/over due date
+  if(moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
+//date picker
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 //sorting
